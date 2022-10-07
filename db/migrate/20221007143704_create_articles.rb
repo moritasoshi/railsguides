ad9e95f24325e1082ae2735136b8b0e66b6f46a0
morita <moritasoushi@gmail.com>
2022-10-08 00:24:29 +0900

    Migration is described in Ruby code

--- db/migrate/20221007143704_create_articles.rb
+++ db/migrate/20221007143704_create_articles.rb
@@ -1,10 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
+    # The create_table adds the `id` column that indicates a primary key
     create_table :articles do |t|
       t.string :title
       t.text :body
 
-      t.timestamps
+      t.timestamps # The timestamps add columns of created_at and updated_at
     end
   end
 end
